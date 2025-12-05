08c009917a644921fadfa98cb6cffee23b435b73
Maxim Klaiman <klaiman@usc.edu>
2025-12-05 22:41:55 +0000

    final commit

--- schedwork.cpp
+++ schedwork.cpp
@@ -9,75 +9,57 @@
 #endif
 
 #include "schedwork.h"
-
 using namespace std;
 
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-bool backtrackHelp(
-    const AvailabilityMatrix& avail,
-    size_t dNeed,
-    size_t mShifts,
-    DailySchedule& sched,
-    vector<int>& used,
+bool go(
+    const AvailabilityMatrix& a,
+    size_t d,
+    size_t m,
+    DailySchedule& s,
+    vector<int>& u,
     int day,
     int slot
 )
 {
-    int n = avail.size();
-    int k = avail[0].size();
+    int n = a.size();
+    int k = a[0].size();
 
-    
-    if(day == n) {
-        return true;
+    if(day >= n) return true;
+
+    if(slot >= (int)d) {
+        return go(a,d,m,s,u,day+1,0);
     }
 
-    
-    if(slot == (int)dNeed) {
-        return backtrackHelp(avail, dNeed, mShifts, sched, used, day+1, 0);
-    }
+    for(int w = 0; w < k; w++){
+        int t1 = w; 
+        int t2 = u[w]; 
 
-    
-    for(int w = 0; w < k; w++) {
+        if(a[day][t1] == false) continue;
 
-        
-        if(!avail[day][w]) {
-            continue; 
-        }
+        if(t2 >= (int)m) continue;
 
-        
-        if(used[w] >= (int)mShifts) {
-            continue;
-        }
-
-       
-        bool already = false;
-        for(int x = 0; x < (int)sched[day].size(); x++) {
-            if(sched[day][x] == (Worker_T)w) {
-                already = true;
-                break;
+        bool bad = false;
+        for(size_t pp = 0; pp < s[day].size(); pp++){
+            if(s[day][pp] == t1) {
+                bad = true;
             }
         }
-        if(already) continue;
+        if(bad) continue;
 
-        
-        sched[day][slot] = w;
-        used[w]++;
+        int before = u[t1];
+        s[day][slot] = t1;
+        u[t1] = before + 1;
 
-       
-        if(backtrackHelp(avail, dNeed, mShifts, sched, used, day, slot+1)) {
-            return true;
-        }
+        if(go(a,d,m,s,u,day,slot+1)) return true;
 
-        
-        used[w]--;
-        sched[day][slot] = INVALID_ID;
+        u[t1] = before;
+        s[day][slot] = INVALID_ID;
     }
 
-    
     return false;
 }
-
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -86,30 +68,28 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U) {
-        return false; 
-    }
+    if(avail.size() == 0) return false;
 
     int n = avail.size();
     int k = avail[0].size();
 
-    
     sched.clear();
     sched.resize(n);
 
-    
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++){
         sched[i].resize(dailyNeed);
-        for(int j = 0; j < (int)dailyNeed; j++) {
+        for(size_t j = 0; j < dailyNeed; j++){
             sched[i][j] = INVALID_ID;
         }
     }
 
-    
-    vector<int> used(k, 0);
+    vector<int> used(k);
+    for(int i = 0; i < k; i++){
+        used[i] = 0;
+    }
 
-    
-    bool ok = backtrackHelp(avail, dailyNeed, maxShifts, sched, used, 0, 0);
+    bool ok = go(avail, dailyNeed, maxShifts, sched, used, 0, 0);
 
     return ok;
 }
+
